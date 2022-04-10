ddc81fa037125c39898fcaea40fea4945c4c1005
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-04-10 14:29:09 +0300

    cleanup and kindswitch links test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 import pytest
 
+
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--log-level=3")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
     d = webdriver.Chrome(options=options)
     d.implicitly_wait(20)
     d.get('https://www.cian.ru/')
--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,12 +1,44 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 
-from pageObjects.header import Header
+from pageobjects.header import Header
+from pageobjects.kindswitch import KindSwitch
+
 
 def test_title_content(driver: WebDriver):
     assert 'Циан - база недвижимости' in driver.title
 
-def test_header_links_are_present(driver: WebDriver):
+
+def test_header_links(driver: WebDriver):
+    expected_links_list = [
+        'Аренда',
+        'Продажа',
+        'Новостройки',
+        'Дома и участки',
+        'Коммерческая',
+        'Ипотека',
+        'Сервисы',
+        'Ещё',
+        'Ещё',
+        'Ещё'
+    ]
     header = Header(driver)
-    els = header.root.find_elements(By.CSS_SELECTOR, 'ul > li')
-    assert len(els) == 10
+    links = [el.get_attribute('textContent') for el in header.root.find_elements(
+        By.CSS_SELECTOR, 'ul > li > *')]
+    assert links == expected_links_list
+
+
+def test_operation_kinds(driver: WebDriver):
+    expected_links_list = [
+        'Купить',
+        'Снять',
+        'Посуточно',
+        'Оценить',
+        'Ипотека',
+        'Подбор риелтора',
+    ]
+    kind_switch = KindSwitch(driver)
+    links = [el.get_attribute('textContent') for el in kind_switch.root.find_elements(
+        By.CSS_SELECTOR, 'ul > li > a')]
+    assert links == expected_links_list
