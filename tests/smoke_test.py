6ef7ac71e3855610535a698a1d5b90f68eea3a68
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-05-18 18:47:35 +0300

    compare promo test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,12 +1,22 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-import helpers.elements as els
+import helpers.elements as eh
 from pageobjects.searchBlock import SearchBlock
 
 
 def test_title_content(driver: WebDriver):
     assert 'Циан - база недвижимости' in driver.title
+
+
+def test_compare_promo_popup(driver: WebDriver):
+    loc = '[data-name="CompareOnboarding"]'
+    eh.check_element_is_present(driver, loc)
+    onb_text = driver.find_element(
+        By.CSS_SELECTOR, loc).get_attribute('textContent')
+    assert 'Сравнивайте квартиры по параметрам' in onb_text
+    assert 'Пока это работает только с квартирами от' in onb_text
+    assert 'застройщиков' in onb_text
 
 
 def test_cookies_notification_hides_on_confirmation(driver: WebDriver):
@@ -17,7 +27,7 @@ def test_cookies_notification_hides_on_confirmation(driver: WebDriver):
     el: WebElement = elements[0].find_element(
         By.XPATH, '//span[text()="Принять"]')
     el.click()
-    els.check_element_is_not_present(
+    eh.check_element_is_not_present(
         driver, '[data-name="CookiesNotification"]')
 
 
