9a5af134feccd091bce84a8a7c1c14ff8cea1c76
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-06-09 11:20:09 +0300

    footer sections test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -118,3 +118,12 @@ def test_specialist_lists_content(driver: WebDriver):
     check.is_in('Риелторы', spec_list[0].get_attribute('textContent'))
     check.is_in('Агентства', spec_list[1].get_attribute('textContent'))
     check.is_in('Застройщики', spec_list[2].get_attribute('textContent'))
+
+def test_footer_sections(driver: WebDriver):
+    footer_loc = '//main/following-sibling::div[1]'
+    text = driver.find_element(By.XPATH, footer_loc).get_attribute('textContent')
+
+    check.is_in('Квартиры у метро', text);
+    check.is_in('Квартиры в районе', text);
+    check.is_in('Недвижимость в Подмосковье', text);
+    check.is_in('Объявления по всей России', text);
