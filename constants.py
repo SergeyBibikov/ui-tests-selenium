6b0cff0fcb9d1578e99f3a9ab9bb27bd06f70720
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-06-25 09:54:56 +0300

    rent recomment quick links test

--- constants.py
+++ constants.py
@@ -2,4 +2,5 @@ urls = {
     "MAIN_PAGE": "https://www.cian.ru/",
     "NEW_HOUSES": "https://www.cian.ru/novostrojki/",
     "COUNTRY_PROPERTY": "https://www.cian.ru/zagorod/",
+    "ROOM_RENT": "https://www.cian.ru/snyat-komnatu/",
 }
