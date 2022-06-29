db3908c00b12f50851707d3d505cc477f8073b69
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-06-29 12:44:35 +0300

    mortgage survey taken test

--- tests/mortgage/mortgage_smoke_test.py
+++ tests/mortgage/mortgage_smoke_test.py
@@ -14,3 +14,11 @@ def test_survey_for_new_users(driver_no_link: WebDriver):
     w.wait_for_text(d, 30, By.CSS_SELECTOR, 'body', "Ответьте на несколько вопросов")
     eh.check_element_is_present(d, '//button[text()="Ок, я готов"]', By.XPATH)
     eh.check_element_is_present(d, '//button[text()="Я уже заполнял анкету"]', By.XPATH)
+
+def test_survey_already_taken(driver_no_link: WebDriver):
+    d = driver_no_link
+
+    d.get(constants.urls["MORTGAGE"])
+    d.find_element(By.XPATH, '//button[text()="Я уже заполнял анкету"]').click()
+    w.wait_for_text(d, 30, By.CSS_SELECTOR, 'body', 'Льготная ипотека от 6 банков')
+    w.wait_for_text(d, 30, By.CSS_SELECTOR, 'body', 'Укажите номер телефона, чтобы продолжить')
