3463d055ae67252ddc43c55cacf7d572db4a9f08
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-07-12 10:28:21 +0300

    place ad rules test

--- tests/place_ad_test.py
+++ tests/place_ad_test.py
@@ -14,6 +14,7 @@ import helpers.waits as w
 OWNER = '//div[text()="Собственник"]'
 HOMEOWNER_TOOLTIP = '//homeowner-tooltip//div'
 RULES_MODAL = '.cui-modal__dialog'
+RULES_LIST = '//ul[@class="modal-rules__list"][1]'
 DOCUMENTS_LIST = '//ul[@class="modal-rules__list"][2]'
 PLACEMENT_RULES = '//a[text()="Правила размещения"]'
 
@@ -47,3 +48,24 @@ def test_rules_modal_should_include_a_warning(driver_place_ad: WebDriver):
             "Объявления, нарушающие данные требования, могут быть отклонены"
             " или удалены без каких-либо компенсаций. При многократных нарушениях "
             "вы получите предупреждение, и ваш аккаунт может быть заблокирован!"))
+
+def test_rules_list(driver_place_ad: WebDriver):
+    d = driver_place_ad
+    d.find_element(By.XPATH, PLACEMENT_RULES).click()
+
+    w.wait_for_text(d, 30, By.CSS_SELECTOR,
+        RULES_MODAL, "Подавая объявление, пожалуйста, убедитесь, что:")
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'правильно выбран раздел для размещения')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'указаны точные и действительные параметры объекта')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'объект реально существует и предложение по нему актуально')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'указана реальная цена объекта, в которую включены все обязательные дополнительные платежи')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'цена указана в значении «до торга», т.е. не учтены скидки и возможные личные договорённости')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'указаны верные условия оплаты (предоплата, залог) и точный размер комиссии')
+    w.wait_for_text(d, 30, By.XPATH,
+        RULES_LIST, 'использованы реальные фотографии объекта, и у вас есть права на их использование')
