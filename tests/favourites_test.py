57e2ea31ced4d79b1ce74d32d95f42350a2812fb
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-07-12 13:52:51 +0300

    favs tests skeleton

---
+++ tests/favourites_test.py
@@ -0,0 +1,28 @@
+import time
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+
+import pytest_check as check
+
+from pageobjects.header import Header
+from pageobjects.base import Base
+import helpers.scripts as s
+import helpers.elements as eh
+import helpers.waits as w
+
+def test_popup_when_ad_is_added_to_favs(driver: WebDriver):
+    pass
+
+def test_popup_when_ad_is_unfaved(driver: WebDriver):
+    pass
+
+def test_popup_on_deletion_from_favs(driver: WebDriver):
+    pass
+
+def test_sort_order_criteria(driver: WebDriver):
+    pass
+
+def test_action_buttons_on_the_ad_in_favs(driver: WebDriver):
+    pass
+
