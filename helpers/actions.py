7b669fde9945ab3f42d63ded27762b126b7ecb80
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-07-18 01:34:22 +0300

    favs page action buttons

--- helpers/actions.py
+++ helpers/actions.py
@@ -5,3 +5,11 @@ from selenium.webdriver.remote.webelement import WebElement
 
 def hover(driver: WebDriver, element: WebElement):
     ActionChains(driver).move_to_element(element).perform()
+
+def switchToNthTab(driver: WebDriver, tab_count: int):
+    """
+        Switches to nth-tab, starting with 1.
+        To switch to the second tab:
+        actions.switchTonNthTab(driver, 2)
+    """
+    driver.switch_to.window(driver.window_handles[tab_count-1])
--- tests/favourites_test.py
+++ tests/favourites_test.py
@@ -3,12 +3,21 @@ import time
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
+import pytest_check as check
+
 from pageobjects.header import Header
 from pageobjects.base import Base
 from pageobjects.searchresults import SearchResults
 import helpers.elements as eh
 import helpers.waits as w
+import helpers.actions as actions
 
+buttons = {
+    "ADD_TO_FOLDER":"[data-name='AddToFolderButtonContainer']",
+    "NOTE":"[data-name='NoteButton']",
+    "REMOVE":"[data-name='RemoveFavorite']",
+    "REPORT":"button[title='Пожаловаться']"
+}
 
 def test_popup_when_ad_is_added_to_favs(driver_rent: WebDriver):
     d = driver_rent
@@ -25,26 +34,32 @@ def test_popup_when_ad_is_unfaved(driver_rent: WebDriver):
 
     SearchResults.toggle_first_result_favs(d)
     SearchResults.toggle_first_result_favs(d)
-    
+
     eh.check_element_is_present(d,'//span[text()="Удалено из избранного"]',By.XPATH)
     eh.check_element_is_present(d,'//p[text()="Объявление удалено из избранного и всех подборок"]',By.XPATH)
     eh.check_element_is_present(d,'//button[span[text()="Понятно"]]',By.XPATH)
 
 
-def test_popup_on_deletion_from_favs(driver: WebDriver):
+def test_popup_on_deletion_from_favs(driver_no_link: WebDriver):
     pass
 
 
-def test_sort_order_criteria(driver: WebDriver):
+def test_sort_order_criteria(driver_no_link: WebDriver):
     """  
     Should have two criferia 
     """
     pass
 
 
-def test_action_buttons_on_the_ad_in_favs(driver: WebDriver):
-    """ 
-    Checks that there are buttons allowing
-    various actions on an item in favs
-    """
-    pass
+def test_action_buttons_on_the_ad_in_favs(driver_rent: WebDriver):
+    d = driver_rent
+
+    SearchResults.toggle_first_result_favs(d)
+    header = Header(d)
+    header.favourites.click()
+    actions.switchToNthTab(d, 2)
+
+    eh.check_element_is_present(d, buttons["ADD_TO_FOLDER"])
+    eh.check_element_is_present(d, buttons["NOTE"])
+    eh.check_element_is_present(d, buttons["REMOVE"])
+    eh.check_element_is_present(d, buttons["REPORT"])
