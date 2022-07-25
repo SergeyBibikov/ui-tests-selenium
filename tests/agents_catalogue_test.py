92c86d15f54ed1080d688a803abcb0a50ce85a02
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-07-25 21:08:39 +0300

    agents list call us banner

--- tests/agents_catalogue_test.py
+++ tests/agents_catalogue_test.py
@@ -15,6 +15,8 @@ import helpers.actions as actions
 
 SORT_FILTER = '[data-name="SortFilter"]'
 FILTER_DROPDOWN = '//div[contains(@class, "dropdown") and contains(., "По умолчанию")]'
+CALL_US_BANNER = '//div[@data-name="CallUsBanner"]/following-sibling::div[1]'
+CALL_BUTTON = '//button[span[text()="+7 800 511-79-..."]]'
 
 def test_agents_list_sorting_filters(driver_no_link: WebDriver):
     d = driver_no_link
@@ -30,3 +32,16 @@ def test_agents_list_sorting_filters(driver_no_link: WebDriver):
     check.is_in('По времени на Циан', text)
     check.is_in('По алфавиту', text)
     check.is_in('По количеству объявлений', text)
+
+def test_call_us_banner_is_present(driver_no_link: WebDriver):
+    d = driver_no_link
+
+    d.get(c.urls["AGENTS_LIST"])
+
+    text = d.find_element(By.XPATH, CALL_US_BANNER).get_attribute('textContent')
+    
+    check.is_in('Позвоните нам', text)
+    check.is_in('Мы бесплатно подберём риелтора по вашим запросам', text)
+    check.is_in('Для звонков по России', text)
+
+    eh.check_element_is_present(d, CALL_US_BANNER + CALL_BUTTON, By.XPATH)
