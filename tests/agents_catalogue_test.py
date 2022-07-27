8ab7dc59e61e3d64c727ebbce1b5a35dc7b6a758
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-07-27 19:56:33 +0300

    agent to agencies switch test

--- tests/agents_catalogue_test.py
+++ tests/agents_catalogue_test.py
@@ -13,6 +13,7 @@ import helpers.elements as eh
 import helpers.waits as w
 import helpers.actions as actions
 
+AGENCIES_BUTTON = '//button[span[text()="Агентства"]]'
 AGENT_SELECTION_BANNER = '//*[@data-name="RealtorBannerCard"]'
 CALL_BUTTON = '//button[span[text()="+7 800 511-79-..."]]'
 CALL_US_BANNER = '//div[@data-name="CallUsBanner"]/following-sibling::div[1]'
@@ -20,33 +21,49 @@ DEAL_TYPE_FILTER = '//div[@data-name="DealTypeContainer"]'
 DEAL_FILTER_DROPDOWN = '//div[contains(@class, "dropdown") and contains(., "Любая сделка")]'
 ESTATE_KIND_FILTER = '//div[@data-name="DropdownWrapper"]'
 ESTATE_KIND_DROPDOWN = '//div[contains(@class, "dropdown") and not(contains(., "Любая недвижимость"))]'
+LISTING = '//div[@data-name="ListingLayout"]'
 SEARCH_BAR = '//input[@placeholder="Имя, телефон или название агентства"]'
 SORT_FILTER_DROPDOWN = '//div[contains(@class, "dropdown") and contains(., "По умолчанию")]'
 SORT_FILTER = '[data-name="SortFilter"]'
 SELECT_AGENT_BUTTON = AGENT_SELECTION_BANNER + \
     '//a[span[text()="Подобрать риелтора"]]'
 
+
+def test_agents_agencies_switch(driver_agents_list: WebDriver):
+    d = driver_agents_list
+
+    d.find_element(By.XPATH, AGENCIES_BUTTON).click()
+    check.is_in('agentstva', d.current_url)
+    text = d.find_element(By.XPATH, LISTING).get_attribute('textContent')
+    check.is_in("Этажи Москва", text)
+
+
 def test_deal_kinds_filter(driver_agents_list: WebDriver):
     d = driver_agents_list
     d.find_element(By.XPATH, DEAL_TYPE_FILTER).click()
 
-    text = d.find_element(By.XPATH, DEAL_FILTER_DROPDOWN).get_attribute('textContent')
+    text = d.find_element(
+        By.XPATH, DEAL_FILTER_DROPDOWN).get_attribute('textContent')
     check.is_in('Аренда', text)
     check.is_in('Покупка и продажа', text)
+
 
 def test_estate_kind_filter(driver_agents_list: WebDriver):
     d = driver_agents_list
     d.find_element(By.XPATH, ESTATE_KIND_FILTER).click()
 
-    text = d.find_element(By.XPATH, ESTATE_KIND_DROPDOWN).get_attribute('textContent')
+    text = d.find_element(
+        By.XPATH, ESTATE_KIND_DROPDOWN).get_attribute('textContent')
     check.is_in('Жилая', text)
     check.is_in('Загородная', text)
     check.is_in('Коммерческая', text)
 
+
 def test_search_bar_is_present(driver_agents_list: WebDriver):
     d = driver_agents_list
-    
+
     eh.check_element_is_present(d, SEARCH_BAR, By.XPATH)
+
 
 def test_agents_list_sorting_filters(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -66,7 +83,6 @@ def test_agents_list_sorting_filters(driver_agents_list: WebDriver):
 def test_call_us_banner_is_present(driver_agents_list: WebDriver):
     d = driver_agents_list
 
-
     text = d.find_element(
         By.XPATH, CALL_US_BANNER).get_attribute('textContent')
 
@@ -85,4 +101,3 @@ def test_agent_selection_help_banner_is_present(driver_agents_list: WebDriver):
     w.wait_for_text(d, 20, By.XPATH, AGENT_SELECTION_BANNER,
                     'Ответьте на несколько вопросов и вам позвонят подходящие риелторы')
     eh.check_element_is_present(d, SELECT_AGENT_BUTTON, By.XPATH)
-
