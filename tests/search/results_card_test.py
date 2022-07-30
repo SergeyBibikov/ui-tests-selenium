5a7513fac01fdd823f17d1d01ca571758209b083
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-07-30 15:59:44 +0300

    result to comparison addition test

---
+++ tests/search/results_card_test.py
@@ -0,0 +1,49 @@
+import time
+
+import pytest_check as check
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+
+from pageobjects.searchblock import SearchBlock
+from pageobjects.searchresults import SearchResults
+import helpers.elements as eh
+import constants
+
+COMPARISON_NOTIFICATION = '[data-name="ComparisonNotification"]'
+
+def test_popup_when_ad_is_added_to_favs(driver_rent: WebDriver):
+    d = driver_rent
+
+    SearchResults.toggle_first_result_favs(d)
+
+    eh.check_element_is_present(
+        d, '//div[text()="Сохранено в избранное"]', By.XPATH)
+
+
+def test_popup_when_ad_is_unfaved(driver_rent: WebDriver):
+
+    d = driver_rent
+
+    SearchResults.toggle_first_result_favs(d)
+    SearchResults.toggle_first_result_favs(d)
+
+    eh.check_element_is_present(
+        d, '//span[text()="Удалено из избранного"]', By.XPATH)
+    eh.check_element_is_present(
+        d, '//p[text()="Объявление удалено из избранного и всех подборок"]', By.XPATH)
+    eh.check_element_is_present(
+        d, '//button[span[text()="Понятно"]]', By.XPATH)
+
+def test_popup_when_ad_is_added_to_comparison(driver_buy_flat_results: WebDriver):
+    d = driver_buy_flat_results
+
+    SearchResults.toggle_first_result_comparison(d)
+
+    eh.check_element_is_present(d, COMPARISON_NOTIFICATION)
+
+    text = d.find_element(By.CSS_SELECTOR, COMPARISON_NOTIFICATION).get_attribute('textContent')
+
+    check.is_in('Вы сравниваете 1 квартиру', text)
+    check.is_in('Можно добавить ещё 19', text)
