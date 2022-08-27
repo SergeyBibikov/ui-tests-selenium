5f697a19a9773735eb67e8f980f6c3602bc1d30f
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-08-27 18:09:00 +0300

    deleted outdated banner test

--- tests/agents_catalogue_test.py
+++ tests/agents_catalogue_test.py
@@ -19,13 +19,15 @@ ESTATE_KIND_FILTER = '//div[@data-name="DropdownWrapper"]'
 ESTATE_KIND_DROPDOWN = '//div[contains(@class, "dropdown") and not(contains(., "Любая недвижимость"))]'
 INTRO_SECTION = '//section[contains(@class,"intro")]'
 LISTING = '//div[@data-name="ListingLayout"]'
-RATING_COMMERCIAL_ESTATE_LABEL = '//label[contains(., "Коммерческая недвижимость")]' 
+RATING_COMMERCIAL_ESTATE_LABEL = '//label[contains(., "Коммерческая недвижимость")]'
 RATING_LINK = '//a[span[text()="Ежемесячный рейтинг профессионалов ↗"]]'
 RATING_HEADINGS = '//div[@class="rating__headings"]'
 SEARCH_BAR = '//input[@placeholder="Имя, телефон или название агентства"]'
 SORT_FILTER_DROPDOWN = '//div[contains(@class, "dropdown") and contains(., "По умолчанию")]'
 SORT_FILTER = '[data-name="SortFilter"]'
-SELECT_AGENT_BUTTON = AGENT_SELECTION_BANNER + '//a[span[text()="Подобрать риелтора"]]'
+SELECT_AGENT_BUTTON = AGENT_SELECTION_BANNER + \
+    '//a[span[text()="Подобрать риелтора"]]'
+
 
 def test_monthly_ratings_lead(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -35,12 +37,14 @@ def test_monthly_ratings_lead(driver_agents_list: WebDriver):
 
     check.is_in('info.cian.ru', d.current_url)
 
-    text = d.find_element(By.XPATH, RATING_HEADINGS).get_attribute('textContent')
-    check.is_in('Место',text)
-    check.is_in('Риелторы',text)
-    check.is_in('Агентства',text)
-    check.is_in('Охват',text)
-    check.is_in('млн показов',text)
+    text = d.find_element(
+        By.XPATH, RATING_HEADINGS).get_attribute('textContent')
+    check.is_in('Место', text)
+    check.is_in('Риелторы', text)
+    check.is_in('Агентства', text)
+    check.is_in('Охват', text)
+    check.is_in('млн показов', text)
+
 
 def test_agents_agencies_switch(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -50,6 +54,7 @@ def test_agents_agencies_switch(driver_agents_list: WebDriver):
     text = d.find_element(By.XPATH, LISTING).get_attribute('textContent')
     check.is_in("Этажи Москва", text)
 
+
 def test_deal_kinds_filter(driver_agents_list: WebDriver):
     d = driver_agents_list
     d.find_element(By.XPATH, DEAL_TYPE_FILTER).click()
@@ -58,6 +63,7 @@ def test_deal_kinds_filter(driver_agents_list: WebDriver):
         By.XPATH, DEAL_FILTER_DROPDOWN).get_attribute('textContent')
     check.is_in('Аренда', text)
     check.is_in('Покупка и продажа', text)
+
 
 def test_estate_kind_filter(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -69,10 +75,12 @@ def test_estate_kind_filter(driver_agents_list: WebDriver):
     check.is_in('Загородная', text)
     check.is_in('Коммерческая', text)
 
+
 def test_search_bar_is_present(driver_agents_list: WebDriver):
     d = driver_agents_list
 
     eh.check_element_is_present(d, SEARCH_BAR, By.XPATH)
+
 
 def test_agents_list_sorting_filters(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -88,17 +96,6 @@ def test_agents_list_sorting_filters(driver_agents_list: WebDriver):
     check.is_in('По алфавиту', text)
     check.is_in('По количеству объявлений', text)
 
-def test_call_us_banner_is_present(driver_agents_list: WebDriver):
-    d = driver_agents_list
-
-    text = d.find_element(
-        By.XPATH, CALL_US_BANNER).get_attribute('textContent')
-
-    check.is_in('Позвоните нам', text)
-    check.is_in('Мы бесплатно подберём риелтора по вашим запросам', text)
-    check.is_in('Для звонков по России', text)
-
-    eh.check_element_is_present(d, CALL_US_BANNER + CALL_BUTTON, By.XPATH)
 
 def test_agent_selection_help_banner_is_present(driver_agents_list: WebDriver):
     d = driver_agents_list
@@ -108,6 +105,7 @@ def test_agent_selection_help_banner_is_present(driver_agents_list: WebDriver):
     w.wait_for_text(d, 20, By.XPATH, AGENT_SELECTION_BANNER,
                     'Ответьте на несколько вопросов и вам позвонят подходящие риелторы')
     eh.check_element_is_present(d, SELECT_AGENT_BUTTON, By.XPATH)
+
 
 def test_rating_estate_kind_switch(driver_no_link: WebDriver):
     d = driver_no_link
--- tests/ostrovok_test.py
+++ tests/ostrovok_test.py
@@ -10,8 +10,9 @@ import time
 
 from pageobjects.avito import Locators as l, confirm_location
 
-
+BUSINESS_TRIPS_TAB = '//a[text()="For business trips"]'
 DESTINATION_DROPDOWN = '//div[contains(@class, "Popup-module") and div[text()="Regions"]]'
+HERO = 'section[class*=Hero]'
 SEARCH_BUTTON = '//div[text()="Search"]'
 
 
@@ -23,3 +24,14 @@ def test_destination_dropdown_on_empty_search_attempt(ostrovok: WebDriver):
     d.find_element(By.XPATH, SEARCH_BUTTON).click()
 
     eh.check_element_is_present(d, DESTINATION_DROPDOWN, By.XPATH)
+
+
+def test_business_trips_successful_lead(ostrovok: WebDriver):
+    d = ostrovok
+
+    d.find_element(By.XPATH, BUSINESS_TRIPS_TAB).click()
+    a.switchToNthTab(d, 2)
+
+    check_text('corp.ostrovok.ru', d.current_url)
+    w.wait_for_text(d, 30, By.CSS_SELECTOR, HERO,
+                    'Easy organization of business trips')
