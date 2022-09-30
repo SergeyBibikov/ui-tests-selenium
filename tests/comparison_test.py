d230d35511fb56e660df9c712617b51910263680
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-09-30 16:43:12 +0300

    comp test imports cleanup

--- tests/comparison_test.py
+++ tests/comparison_test.py
@@ -1,18 +1,11 @@
-from pageobjects.header import Header
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.common.action_chains import ActionChains
-
-import helpers.elements as eh
-import helpers.waits as w
-import helpers.actions as a
 
 import pytest_check as check
-import time
 from constants import Urls
 
 SEARCH_FLATS = '//span[text()="Искать квартиры"]'
+
 
 def test_flat_search_button_leads_to_search_page(driver_no_link: WebDriver):
     d = driver_no_link
